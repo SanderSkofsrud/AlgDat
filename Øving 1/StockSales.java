f9866e1cb210a316b7b1b34c36ea7884cf3df558
sande <sander@skofsrud.net>
2023-08-23 12:26:29 +0200

    Changed code back to Arrays from map

--- Øving 1/StockSales.java
+++ Øving 1/StockSales.java
@@ -1,77 +1,74 @@
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Random;
 
 public class StockSales {
-  /*
-   * Map of day and price change
-   */
-  static Map<Integer, Integer> priceChange = new HashMap<>();
-  /*
-   * Random number generator
-   */
-  static Random random = new Random();
 
-  /*
-   * Finds the maximum profit of a stock sale
-   * @param priceChange Map of day and price change
-   * @return String with buy day, sell day and profit
-   */
-  private static String maxProfit(Map<Integer, Integer> priceChange) {
-    if (priceChange.size() == 0) {
-      return "No prices provided";
+    private static Random random = new Random();
+
+    private static String maxProfit(int[] stockPrices) {
+        if (stockPrices.length == 0) {
+            return "No prices provided";
+        }
+
+        int minPrice = stockPrices[0];
+        int minPriceDay = 0;
+        int maxProfit = Integer.MIN_VALUE;
+        int buyDay = 0;
+        int sellDay = 0;
+
+        for (int i = 1; i < stockPrices.length; i++) {
+            int currentPrice = stockPrices[i];
+
+            if (currentPrice - minPrice > maxProfit) {
+                maxProfit = currentPrice - minPrice;
+                buyDay = minPriceDay;
+                sellDay = i;
+            }
+
+            if (currentPrice < minPrice) {
+                minPrice = currentPrice;
+                minPriceDay = i;
+            }
+        }
+
+        return "Buy on day " + buyDay + " and sell on day " + sellDay + " for a profit of $" + maxProfit;
     }
 
-    int minPrice = priceChange.get(0);
-    int minPriceDay = 0;
-    int maxProfit = Integer.MIN_VALUE;
-    int buyDay = 0;
-    int sellDay = 0;
-
-    for (int i = 1; i < priceChange.size(); i++) {
-      int currentPrice = priceChange.get(i);
-
-      if (currentPrice - minPrice > maxProfit) {
-        maxProfit = currentPrice - minPrice;
-        buyDay = minPriceDay;
-        sellDay = i;
-      }
-
-      if (currentPrice < minPrice) {
-        minPrice = currentPrice;
-        minPriceDay = i;
-      }
+    private static int[] generateRandomPriceChanges(int amount) {
+        int[] priceChanges = new int[amount];
+        for (int i = 0; i < amount; i++) {
+            priceChanges[i] = random.nextInt(19) - 9;  // Random numbers between -9 and 9 inclusive.
+        }
+        return priceChanges;
     }
 
-    return "Buy on day " + buyDay + " and sell on day " + sellDay + " for a profit of $" + maxProfit;
-  }
-
-  /*
-   * Main method
-   * Creates a map of 100 days with random price changes
-   * Prints the maximum profit
-   * Prints the time it takes to run the method
-   * @param args Command line arguments
-   * @return void
-   */
-  public static void main(String[] args) {
-    for (int i = 0; i < 100; i++) {
-      int randomNumber = random.nextInt(19) - 9;
-      priceChange.put(i, randomNumber);
+    private static int[] generateStockPrices(int[] priceChanges) {
+        int[] stockPrices = new int[priceChanges.length];
+        int cumulativePrice = 0;
+        for (int i = 0; i < priceChanges.length; i++) {
+            cumulativePrice += priceChanges[i];
+            stockPrices[i] = cumulativePrice;
+        }
+        return stockPrices;
     }
 
-    System.out.println(maxProfit(priceChange));
+    public static void main(String[] args) {
+        testPerformance(100000);
+        testPerformance(1000000);
+        testPerformance(10000000);
+    }
 
-    long startTime = System.nanoTime();
-    maxProfit(priceChange);
-    long endTime = System.nanoTime();
-    long duration = (endTime - startTime)/1000;
-    System.out.println("Time: " + duration + "ms");
+    private static void testPerformance(int days) {
+        int[] priceChanges = generateRandomPriceChanges(days);
+        int[] stockPrices = generateStockPrices(priceChanges);
 
-    long startTime2 = System.nanoTime();
-    maxProfit(priceChange);
-    long endTime2 = System.nanoTime();
-    long duration2 = (endTime2 - startTime2)/1000;
-    System.out.println("Time: " + duration2 + "ms");
-  }
+        System.out.println(days + " days:");
+        System.out.println(maxProfit(stockPrices));
+
+        long startTime = System.nanoTime();
+        maxProfit(stockPrices);
+        long endTime = System.nanoTime();
+        double duration = (endTime - startTime) / 1_000_000.0;  // Convert to milliseconds
+
+        System.out.printf("Time: %.2fms%n", duration);  // Print with 2 decimal places
+    }
 }
